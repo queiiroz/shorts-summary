34c402081cc2828d1dccbc583508d8eed3a72ca5
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-09-14 22:53:45 -0300

    chore: vercel serveless function configuration

--- api/transcricao.js
+++ api/transcricao.js
@@ -1,25 +1,23 @@
-import { transcriptionExample } from "./utils/transcription.js"
-
-import { pipeline } from "@xenova/transformers"
+import { pipeline } from "@xenova/transformers";
 
 export async function transcribe(audio) {
   try {
-    console.log("Realizando a transcrição...")
+    console.log("Realizando a transcrição...");
     const transcribe = await pipeline(
       "automatic-speech-recognition",
       "Xenova/whisper-small"
-    )
+    );
 
     const transcription = await transcribe(audio, {
       chunk_length_s: 30,
       stride_length_s: 5,
       language: "portuguese",
       task: "transcribe",
-    })
+    });
 
-    console.log("Transcrição finalizada")
-    return transcription?.text.replace("[Música]", "")
+    console.log("Transcrição finalizada");
+    return transcription?.text.replace("[Música]", "");
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
 }
